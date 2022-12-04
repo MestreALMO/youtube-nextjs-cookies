c308bb7abd15cddb60bb204dbc85a86f5e32eceb
André Lusegardis <lusegardis@gmail.com>
2022-12-04 15:01:46 -0300

    application done

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { ToggleSiteTheme } from "../componets/toggleSiteTheme";
+
+export default function Home() {
+  return (
+    <>
+      <h1>André Lusegardis, YouTube Channel</h1>
+
+      <ToggleSiteTheme />
+    </>
+  );
+}
